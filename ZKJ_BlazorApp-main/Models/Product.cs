c61bdac7ba0a71523dd0807b808c3ec90e0d54d8
Tomasz Kowalczyk <TomaszKowalczykAGH@gmail.com>
2023-02-24 23:33:15 +0100

    Added Product Resources

---
+++ ZKJ_BlazorApp-main/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BlazorApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public decimal? UnitPriceNetto { get; set; }
+        public decimal? UnitPriceBrutto { get; set; }
+        public float? Discount { get; set; }
+        public DateTime? LastModified { get; set; }
+        
+    }
+}
---
+++ ZKJ_BlazorApp-main/Services/ProductsService.cs
@@ -0,0 +1,44 @@
+﻿using BlazorApp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Services
+{
+    public class ProductsService : IProductsService
+    {
+        private IHttpService _httpService;
+
+        public ProductsService(IHttpService httpService)
+        {
+            _httpService = httpService;
+        }
+
+        public Task<IEnumerable<Product>> GetAll()
+        {
+            return _httpService.Get<IEnumerable<Product>>("/products");
+        }
+
+        public async Task<int> Create(Product product)
+        {
+            var result = await _httpService.Post<Product>("/products", product);
+            return result.Id;
+        }
+
+        public async Task<int> Delete(int id)
+        {
+            await _httpService.Delete($"/products/{id}");
+            return id;
+        }
+
+        public async Task<int> Update(Product product)
+        {
+            var result = await _httpService.Put<Product>($"/products/{product.Id}", product);
+            return result.Id;
+        }
+
+        public Task<Product> GetById(int id)
+        {
+            return _httpService.Get<Product>($"/products/{id}");
+        }
+    }
+}
