724f78b0994f66e771ee4198df5d2f74f6573004
Tomasz Kowalczyk <TomaszKowalczykAGH@gmail.com>
2023-03-07 22:13:19 +0100

    resources/contractors, added model and service

---
+++ ZKJ_BlazorApp-main/Models/Contractors.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorApp.Models
+{
+    public class Contractors
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public int NIP { get; set; }
+        public string Country { get; set; }
+
+    }
+}
---
+++ ZKJ_BlazorApp-main/Services/IContractorsService.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorApp.Services
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using BlazorApp.Models;
+
+    public interface IContractorsService
+    {
+        Task<IEnumerable<Contractors>> GetAll();
+        Task<int> Create(Contractors contractor);
+        Task<int> Delete(int Id);
+        Task<int> Update(Contractors contractor);
+        Task<Contractors> GetById(int Id);
+    }
+}
