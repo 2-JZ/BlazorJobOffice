26fb695dd5f25ab4c409fdcdedae2d6cefc39068
Tomasz Kowalczyk <TomaszKowalczykAGH@gmail.com>
2023-03-11 18:31:42 +0100

    resources/Contractors. repaired Http Put

--- ZKJ_BlazorApp-main/Services/ContractorsService.cs
+++ ZKJ_BlazorApp-main/Services/ContractorsService.cs
@@ -14,29 +14,29 @@ namespace BlazorApp.Services
         }
         public async Task<int> Create(Contractor contractor)
         {
-            var result = await _httpService.Post<Contractor>("contractors/addContractor", contractor);
+            var result = await _httpService.Post<Contractor>("Contractors/addContractor", contractor);
             return result.Id;
         }
 
         public async Task<int> Delete(int id)
         {
-            await _httpService.Delete($"/contractors/{id}");
+            await _httpService.Delete($"/Contractors/{id}");
             return id;
         }
 
         public async Task<IEnumerable<Contractor>> GetAll()
         {
-            return await _httpService.Get<IEnumerable<Contractor>>("/contractors");
+            return await _httpService.Get<IEnumerable<Contractor>>("contractors");
         }
 
         public Task<Contractor> GetById(int id)
         {
-            return _httpService.Get<Contractor>($"/contractors/{id}");
+            return _httpService.Get<Contractor>($"/Contractors/{id}");
         }
 
         public async Task<int> Update(Contractor contractor)
         {
-            var result = await _httpService.Put<Contractor>($"/contractors/{contractor.Id}", contractor);
+            var result = await _httpService.Put<Contractor>($"/Contractors/{contractor.Id}", contractor);
             return result.Id;
         }
     }
