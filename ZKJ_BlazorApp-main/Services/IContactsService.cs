dc7409a445b1bbe38c7d201a6a87678bcef0cd78
Tomasz Kowalczyk <TomaszKowalczykAGH@gmail.com>
2023-03-12 21:22:47 +0100

    Added Contacts (Edit, Add, Delete, GetAll) resource

---
+++ ZKJ_BlazorApp-main/Services/IContactsService.cs
@@ -0,0 +1,15 @@
+﻿using BlazorApp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Services
+{
+    public interface IContactsService
+    {
+        Task<IEnumerable<Contact>> GetAll();
+        Task<int> Create(Contact contact);
+        Task<int> Delete(int id);
+        Task<int> Update(Contact contact);
+        Task<Contact> GetById(int id);
+    }
+}
