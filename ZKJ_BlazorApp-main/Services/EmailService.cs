fea8dc693ddbd1be2af5d6da22dd00e1ede53806
2-JZ <tomaskowalcz@gmail.com>
2024-03-14 18:42:07 +0100

    Contact Form and Email Service

---
+++ ZKJ_BlazorApp-main/Services/EmailService.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly EmailClient emailClient;
+
+        public EmailService(EmailClient emailClient)
+        {
+            this.emailClient = emailClient;
+        }
+
+        public async Task SendEmailAsync(string from, string to, string subject, string message)
+        {
+            var fromEmailAddress = new EmailAddress(from);
+            var toEmailAddress = new EmailAddress(to);
+
+            await emailClient.SendAsync(Azure.WaitUntil.Started, fromEmailAddress.Address, toEmailAddress.Address, subject, message);
+        }
+
+    }
+}
